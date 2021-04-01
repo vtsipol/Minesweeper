bb872bf03161af2248408db732f47834037432a6
Vasilis Tsipolitis <vasilistsipol@gmail.com>
2021-04-01 12:51:22 +0200

    Board v2.0.5 Create comments for all classes.

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,11 @@ public class Main extends JFrame {
 
         add(new Board(statusbar));
 
-        //False: Not being able to resize the window
+        // False: not being able to resize the window
         setResizable(true);
+        // The pack() method is defined in Window class in Java
+        // and it sizes the frame so that all its contents are at
+        // or above their preferred sizes.
         pack();
 
         setTitle("Minesweeper");
@@ -23,6 +26,10 @@ public class Main extends JFrame {
     }
 
     public static void main(String[] args) {
+        // It posts an event (your Runnable)
+        // at the end of Swings event list and
+        // is processed after all previous GUI
+        // events are processed.
         EventQueue.invokeLater(() -> {
             var ex = new Main();
             ex.setVisible(true);
